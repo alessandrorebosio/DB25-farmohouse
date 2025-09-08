88010086fd23858a3843070e5418f8fdbaf1745d
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-09-08 17:27:21 +0200

    added core app

--- src/main/core/views.py
+++ src/main/core/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
 
 # Create your views here.
+def homepage(request: HttpRequest) -> HttpResponse:
+    return render(request, "index.html")
