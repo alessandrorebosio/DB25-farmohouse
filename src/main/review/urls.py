da95cfb5b3ab510dac201a1a00d8c046112840bf
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-09-16 12:55:05 +0200

    added review app in django

--- src/main/review/urls.py
+++ src/main/review/urls.py
@@ -15,15 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
-    path("admin/", admin.site.urls, name="admin"),
-    path("", include("core.urls")),
-    path("", include("users.urls")),
-    path("", include("product.urls")),
-    path("", include("event.urls")),
-    path("", include("service.urls")),
-    path("", include("review.urls")),
+    
 ]
