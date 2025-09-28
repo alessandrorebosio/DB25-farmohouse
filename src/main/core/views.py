92283a5d53817f4920b2f86b7f1be05346d64086
Alessandro Rebosio <alessandro.rebosio@studio.unibo.it>
2025-09-28 15:27:31 +0200

    refactoring homepage

--- src/main/core/views.py
+++ src/main/core/views.py
@@ -4,4 +4,7 @@ from django.http import HttpRequest, HttpResponse
 
 # Create your views here.
 def homepage(request: HttpRequest) -> HttpResponse:
+    """
+    Renders the "index.html" page when a request is made to the homepage.
+    """
     return render(request, "index.html")
